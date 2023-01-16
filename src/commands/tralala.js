7bcdc425388da90a7c790bdca59b2b635708b16d
mstream <maciej.laciak@gmail.com>
2023-01-16 07:04:44 +0000

    feat: add "tralala" command

---
+++ src/commands/tralala.js
@@ -0,0 +1,24 @@
+const {
+  EmbedBuilder,
+  SlashCommandBuilder,
+} = require(`discord.js`)
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName(`tralala`)
+    .setDescription(`Touch Günther's tralala.`),
+
+  async execute(
+    _1,
+    interaction,
+  ) {
+    interaction.reply({
+      content: `Ooh, you touched my thralala. Mmm, my ding ding dong.`,
+      embeds: [
+        new EmbedBuilder()
+          .setImage(`https://media.tenor.com/lIVHCtkYKlAAAAAC/tralala-gunther.gif`)
+      ],
+      tts: true,
+    })
+  },
+}
